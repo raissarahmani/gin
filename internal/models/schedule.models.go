437b2fbcd3f3faf15dab5eb357664d2977f577fb
rrhmn <raissarahmani18@gmail.com>
2025-04-28 15:44:06 +0700

    fix: schedule struct

--- internal/models/schedule.models.go
+++ internal/models/schedule.models.go
@@ -4,7 +4,6 @@ import "time"
 
 type Schedule struct {
 	Id int `db:"id" json:"id"`
-	// User     int       `db:"users_id" json:"user_id"`
 	Movie    string    `db:"movies_id" json:"title"`
 	Date     time.Time `db:"book_date" json:"date"`
 	Time     time.Time `db:"order_time_id" json:"time"`
