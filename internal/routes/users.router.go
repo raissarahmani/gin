a2e98356910f8d922571a5787df487cf89f199f3
rrhmn <raissarahmani18@gmail.com>
2025-05-03 16:19:15 +0700

    feat: add DI

--- internal/routes/users.router.go
+++ internal/routes/users.router.go
@@ -2,14 +2,18 @@ package routes
 
 import (
 	"main/internal/handlers"
+	"main/internal/repositories"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
-func initUserRouter(router *gin.Engine) {
-	userHandler := handlers.NewUserHandler()
+func initUserRouter(router *gin.Engine, pg *pgxpool.Pool, rdc *redis.Client) {
+	userRepo := repositories.NewUserRepository(pg, rdc)
+	userHandler := handlers.NewUserHandler(userRepo)
 
-	userRouter := router.Group("/users") 
+	userRouter := router.Group("/users")
 	{
 		userRouter.POST("/register", userHandler.RegistUser)
 		userRouter.POST("/login", userHandler.Login)
