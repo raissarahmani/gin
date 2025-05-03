b099db2e963a5a4d13044a9c2c40c037ee4c9e77
raissarahmani <157332071+raissarahmani@users.noreply.github.com>
2025-05-03 17:14:13 +0700

    Delete internal/models/cinema.models.go

--- internal/models/cinema.models.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Cinema struct {
-	Id       int    `db:"id" json:"id"`
-	Location string `db:"order_id" json:"city"`
-	Cinema   string `db:"cinema_name_id" json:"cinema_name"`
-	Studio   string `db:"studio_type" json:"studio"`
-	Price    int    `db:"price" json:"price"`
-}
