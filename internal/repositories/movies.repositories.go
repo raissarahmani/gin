a92a7774dec2e9ee13f5918ee254245a7f5ba1dd
rrhmn <raissarahmani18@gmail.com>
2025-05-04 00:42:08 +0700

    add DI, fix query due to change in db, add pagination for all movie, caching in redis, add new handler for add,edit,delete movie

--- internal/repositories/movies.repositories.go
+++ internal/repositories/movies.repositories.go
@@ -2,25 +2,58 @@ package repositories
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"log"
 	"main/internal/models"
-	"main/pkg"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
-type MovieRepositories struct{}
-
-var MovieRepo *MovieRepositories
-
-func NewMovieRepository() {
-	MovieRepo = &MovieRepositories{}
+type MovieRepositories struct {
+	db  *pgxpool.Pool
+	rdb *redis.Client
 }
 
-func (m *MovieRepositories) ShowAllMovies(c context.Context) ([]models.Movies, error) {
+func NewMovieRepository(pg *pgxpool.Pool, rdc *redis.Client) *MovieRepositories {
+	return &MovieRepositories{
+		db:  pg,
+		rdb: rdc,
+	}
+}
+
+func (m *MovieRepositories) ShowAllMovies(c context.Context, limit, offset int) ([]models.Movies, error) {
+	// cek redis terlebih dahulu, jika ada nilainya maka gunakan nilai dari redis
+	redisKey := fmt.Sprintf("movies:limit=%d:offset=%d", limit, offset)
+	cache, err := m.rdb.Get(c, redisKey).Result()
+	if err != nil {
+		if err == redis.Nil {
+			log.Printf("\nkey %s does not exist\n", redisKey)
+		} else {
+			log.Println("Redis is not working")
+		}
+	} else {
+		var movies []models.Movies
+		if err := json.Unmarshal([]byte(cache), &movies); err != nil {
+			return nil, err
+		}
+		if len(movies) > 0 {
+			return movies, nil
+		}
+	}
+
 	query := `
-		SELECT m.id, mi.poster, m.title, mg.genre_name
-		FROM movies m
-		JOIN movies_genre mg ON m.movies_genre_id = mg.id
-		JOIN movies_image mi ON m.movies_image_id = mi.id`
-	rows, err := pkg.Database.Query(c, query)
+		SELECT m.id, mi.poster, m.title, string_agg(g.genre_name, ', ') as genre
+		from movies m
+		join movies_genre mg on mg.movies_id = m.id
+		join genre g on mg.genre_id = g.id
+		join movies_image mi on m.movies_image_id = mi.id
+		group by m.id, m.title, mi.poster
+		order by m.id limit $1 offset $2`
+
+	rows, err := m.db.Query(c, query, limit, offset)
 	if err != nil {
 		return nil, err
 	}
@@ -29,25 +62,34 @@ func (m *MovieRepositories) ShowAllMovies(c context.Context) ([]models.Movies, e
 	var movies []models.Movies
 	for rows.Next() {
 		var movie models.Movies
-		if err := rows.Scan(&movie.Id, &movie.Image, &movie.Title, &movie.Genre); err != nil {
+		if err := rows.Scan(&movie.Id, &movie.Title, &movie.Image, &movie.Genre); err != nil {
 			return nil, err
 		}
 		movies = append(movies, movie)
 	}
 
+	res, err := json.Marshal(movies)
+	if err != nil {
+		log.Println("[DEBUG] marshal", err.Error())
+	}
+	if err := m.rdb.Set(c, redisKey, string(res), time.Minute*5).Err(); err != nil {
+		log.Println("[DEBUG] redis set", err.Error())
+	}
 	return movies, nil
 }
 
 func (m *MovieRepositories) ShowMovieDetail(c context.Context, id int) (models.Movies, error) {
 	query := `
-		SELECT m.id, mi.poster, m.title, mg.genre_name, m.duration, m.release_date, m.director, m.casts, m.synopsis
-		FROM movies m
-		JOIN movies_genre mg ON m.movies_genre_id = mg.id
-		JOIN movies_image mi ON m.movies_image_id = mi.id
+		SELECT m.id, mi.poster, m.title, string_agg(g.genre_name, ', ') as genre, m.duration, m.release_date, m.director, m.casts, m.synopsis
+		from movies m
+		join movies_genre mg on mg.movies_id = m.id
+		join genre g on mg.genre_id = g.id
+		join movies_image mi on m.movies_image_id = mi.id
+		group by m.id, m.title, mi.poster
 		WHERE m.id = $1`
 
 	var detail models.Movies
-	err := pkg.Database.QueryRow(c, query, id).Scan(&detail.Id, &detail.Image, &detail.Title, &detail.Genre, &detail.Duration, &detail.Release_date, &detail.Director, &detail.Casts, &detail.Synopsis)
+	err := m.db.QueryRow(c, query, id).Scan(&detail.Id, &detail.Image, &detail.Title, &detail.Genre, &detail.Duration, &detail.Release_date, &detail.Director, &detail.Casts, &detail.Synopsis)
 
 	if err != nil {
 		return models.Movies{}, err
@@ -57,12 +99,15 @@ func (m *MovieRepositories) ShowMovieDetail(c context.Context, id int) (models.M
 
 func (m *MovieRepositories) FilterMoviesByTitle(c context.Context, title string) ([]models.Movies, error) {
 	query := `
-		SELECT m.id, mi.poster, m.title, mg.genre_name
-		FROM movies m
-		JOIN movies_genre mg ON m.movies_genre_id = mg.id
-		JOIN movies_image mi ON m.movies_image_id = mi.id
+		SELECT m.id, mi.poster, m.title, string_agg(g.genre_name, ', ') as genre
+		from movies m
+		join movies_genre mg on mg.movies_id = m.id
+		join genre g on mg.genre_id = g.id
+		join movies_image mi on m.movies_image_id = mi.id
+		group by m.id, m.title, mi.poster
+		order by m.id
 		WHERE LOWER(m.title) = LOWER($1)`
-	rows, err := pkg.Database.Query(c, query, title)
+	rows, err := m.db.Query(c, query, title)
 	if err != nil {
 		return nil, err
 	}
@@ -82,12 +127,15 @@ func (m *MovieRepositories) FilterMoviesByTitle(c context.Context, title string)
 
 func (m *MovieRepositories) FilterMoviesByGenre(c context.Context, genreName string) ([]models.Movies, error) {
 	query := `
-		SELECT m.id, mi.poster, m.title, mg.genre_name
-		FROM movies m
-		JOIN movies_genre mg ON m.movies_genre_id = mg.id
-		JOIN movies_image mi ON m.movies_image_id = mi.id
-		WHERE LOWER(mg.genre_name) = LOWER($1)`
-	rows, err := pkg.Database.Query(c, query, genreName)
+		SELECT m.id, mi.poster, m.title, string_agg(g.genre_name, ', ') as genre
+		from movies m
+		join movies_genre mg on mg.movies_id = m.id
+		join genre g on mg.genre_id = g.id
+		join movies_image mi on m.movies_image_id = mi.id
+		group by m.id, m.title, mi.poster
+		order by m.id
+		WHERE LOWER(g.genre_name) = LOWER($1)`
+	rows, err := m.db.Query(c, query, genreName)
 	if err != nil {
 		return nil, err
 	}
@@ -107,13 +155,16 @@ func (m *MovieRepositories) FilterMoviesByGenre(c context.Context, genreName str
 
 func (m *MovieRepositories) FilterMoviesByTitleAndGenre(c context.Context, title, genre string) ([]models.Movies, error) {
 	query := `
-		SELECT m.id, mi.poster, m.title, mg.genre_name
-		FROM movies m
-		JOIN movies_genre mg ON m.movies_genre_id = mg.id
-		JOIN movies_image mi ON m.movies_image_id = mi.id
-		WHERE LOWER(m.title) = LOWER($1) AND LOWER(mg.genre_name) = LOWER($2)`
+		SELECT m.id, mi.poster, m.title, string_agg(g.genre_name, ', ') as genre
+		from movies m
+		join movies_genre mg on mg.movies_id = m.id
+		join genre g on mg.genre_id = g.id
+		join movies_image mi on m.movies_image_id = mi.id
+		group by m.id, m.title, mi.poster
+		order by m.id
+		WHERE LOWER(m.title) = LOWER($1) AND LOWER(g.genre_name) = LOWER($2)`
 	var result []models.Movies
-	rows, err := pkg.Database.Query(c, query, title, genre)
+	rows, err := m.db.Query(c, query, title, genre)
 	if err != nil {
 		return nil, err
 	}
@@ -131,12 +182,15 @@ func (m *MovieRepositories) FilterMoviesByTitleAndGenre(c context.Context, title
 
 func (m *MovieRepositories) ShowUpcomingMovies(c context.Context) ([]models.Movies, error) {
 	query := `
-		SELECT m.id, mi.poster, m.title, mg.genre_name, m.release_date
-		FROM movies m
-		JOIN movies_genre mg ON m.movies_genre_id = mg.id
-		JOIN movies_image mi ON m.movies_image_id = mi.id
+		SELECT m.id, mi.poster, m.title, string_agg(g.genre_name, ', ') as genre, m.release_date
+		from movies m
+		join movies_genre mg on mg.movies_id = m.id
+		join genre g on mg.genre_id = g.id
+		join movies_image mi on m.movies_image_id = mi.id
+		group by m.id, m.title, mi.poster
+		order by m.id
 		WHERE m.release_date > now()`
-	rows, err := pkg.Database.Query(c, query)
+	rows, err := m.db.Query(c, query)
 	if err != nil {
 		return nil, err
 	}
@@ -151,33 +205,28 @@ func (m *MovieRepositories) ShowUpcomingMovies(c context.Context) ([]models.Movi
 
 		movies = append(movies, movie)
 	}
-
 	return movies, nil
 }
 
-func (m *MovieRepositories) GetSchedulesByMovieID(c context.Context, movie_id int) ([]models.Schedule, error) {
+func (m *MovieRepositories) AddNewMovie(c context.Context, movie models.Movies) error {
 	query := `
-		SELECT o.id, m.title, o.book_date, ot.book_time, ol.location 
-		FROM "order" o
-		JOIN movies m ON o.movies_id = m.id
-		JOIN order_time ot ON o.order_time_id = ot.id
-		JOIN order_location ol ON o.order_location_id = ol.id
-		WHERE m.id = $1`
-	rows, err := pkg.Database.Query(c, query, movie_id)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
+		INSERT INTO movies (movies_image_id, title, duration, release_date, director, casts, synopsis)
+		VALUES ($1, $2, $3, $4, $5, $6, $7)`
+	_, err := m.db.Exec(c, query, movie.Image, movie.Title, movie.Duration, movie.Release_date, movie.Director, movie.Casts, movie.Synopsis)
+	return err
+}
 
-	var schedules []models.Schedule
+func (m *MovieRepositories) EditMovie(c context.Context, id int, movie models.Movies) error {
+	query := `
+		UPDATE movies
+		SET movies_image_id=$1, title=$2, duration=$3, release_date=$4, director=$5, casts=$6, synopsis=$7
+		WHERE id=$8`
+	_, err := m.db.Exec(c, query, movie.Image, movie.Title, movie.Duration, movie.Release_date, movie.Director, movie.Casts, movie.Synopsis, id)
+	return err
+}
 
-	for rows.Next() {
-		var schedule models.Schedule
-		if err := rows.Scan(&schedule.Id, &schedule.Movie, &schedule.Date, &schedule.Time, &schedule.Location); err != nil {
-			return nil, err
-		}
-		schedules = append(schedules, schedule)
-	}
-
-	return schedules, nil
+func (m *MovieRepositories) DeleteMovie(c context.Context, id int) error {
+	query := `DELETE FROM movies WHERE id=$1`
+	_, err := m.db.Exec(c, query, id)
+	return err
 }
