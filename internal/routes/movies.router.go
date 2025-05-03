2d6e7b54201634838308de20f38c4fb321e55ae1
rrhmn <raissarahmani18@gmail.com>
2025-05-04 00:37:45 +0700

    add: admin route for add, edit, delete movie

--- internal/routes/movies.router.go
+++ internal/routes/movies.router.go
@@ -2,19 +2,28 @@ package routes
 
 import (
 	"main/internal/handlers"
+	"main/internal/middlewares"
+	"main/internal/repositories"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
-func initMovieRouter(router *gin.Engine) {
-	movieHandler := handlers.NewMovieHandler()
+func initMovieRouter(router *gin.Engine, pg *pgxpool.Pool, rdc *redis.Client, mw *middlewares.Middleware) {
+	movieRepo := repositories.NewMovieRepository(pg, rdc)
+	movieHandler := handlers.NewMovieHandler(movieRepo)
 
 	movieRouter := router.Group("/movies")
-	{
-		movieRouter.GET("", movieHandler.AllMovies)
-		movieRouter.GET("/:id", movieHandler.MovieDetail)
-		movieRouter.GET("/filter", movieHandler.FilterMovies)
-		movieRouter.GET("/upcoming", movieHandler.UpcomingMovies)
-		movieRouter.GET("/:id/schedule", movieHandler.GetMovieSchedules)
-	}
+	movieRouter.GET("", movieHandler.AllMovies)
+	movieRouter.GET("/:id", movieHandler.MovieDetail)
+	movieRouter.GET("/filter", movieHandler.FilterMovies)
+	movieRouter.GET("/upcoming", movieHandler.UpcomingMovies)
+
+	adminRouter := router.Group("/admin/movies")
+	adminRouter.Use(mw.AccessGateAdmin)
+	adminRouter.POST("", movieHandler.AddMovie)
+	adminRouter.PATCH("/:id", movieHandler.EditMovie)
+	adminRouter.DELETE("/:id", movieHandler.DeleteMovie)
+
 }
