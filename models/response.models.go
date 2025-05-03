1f8c366f24620d4ed441ed3ed1476394baa2e001
raissarahmani <157332071+raissarahmani@users.noreply.github.com>
2025-05-03 17:11:37 +0700

    Delete models directory

--- models/response.models.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Response struct {
-	Msg  string `json:"message"`
-	Data any    `json:"data"`
-}
--- models/users.models.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Users struct {
-	Email    string `db:"email" json:"email"`
-	Password string `db:"password" json:"password"`
-}
