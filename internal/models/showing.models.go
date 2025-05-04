0dc6509161665afefc89a15f75c91ed6d3c6632f
rrhmn <raissarahmani18@gmail.com>
2025-05-04 15:34:53 +0700

    fix: add more id

--- internal/models/showing.models.go
+++ internal/models/showing.models.go
@@ -3,11 +3,16 @@ package models
 import "time"
 
 type Schedule struct {
-	Id     int       `db:"id" json:"id"`
-	Date   time.Time `db:"schedule_id" json:"date,omitempty"`
-	Time   time.Time `db:"schedule_id" json:"time,omitempty"`
-	City   string    `db:"city_id" json:"city,omitempty"`
-	Cinema string    `db:"cinema_id" json:"cinema,omitempty"`
-	Movie  string    `db:"movies_id" json:"title,omitempty"`
-	Seat   string    `db:"seat_id" json:"seat,omitempty"`
+	Id         int       `db:"id" json:"id"`
+	Date       time.Time `db:"book_date" json:"date,omitempty"`
+	Time       time.Time `db:"book_time" json:"time,omitempty"`
+	City       string    `db:"city" json:"city,omitempty"`
+	Cinema     string    `db:"cinema_name" json:"cinema,omitempty"`
+	Movie      string    `db:"title" json:"title,omitempty"`
+	Seat       string    `db:"seat_number" json:"seat,omitempty"`
+	MovieID    int       `json:"movie_id,omitempty"`
+	CityID     int       `json:"city_id,omitempty"`
+	CinemaID   int       `json:"cinema_id,omitempty"`
+	ScheduleID int       `json:"schedule_id,omitempty"`
+	SeatID     int       `json:"seat_id,omitempty"`
 }
