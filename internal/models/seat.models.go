059c8c1a352f88bfbd8bf5a29c567177f5f53436
raissarahmani <157332071+raissarahmani@users.noreply.github.com>
2025-06-15 18:09:26 +0700

    Delete internal/models/seat.models.go

--- internal/models/seat.models.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Seat struct {
-	Id           int      `db:"id" json:"id"`
-	Cinema       int      `db:"cinema_id" json:"cinema_id"`
-	Seat_number  []string `db:"seat_number" json:"seat_number"`
-	Available    int      `db:"seat_available" json:"available"`
-	Jumlah_kursi int      `db:"qty" json:"qty"`
-}
