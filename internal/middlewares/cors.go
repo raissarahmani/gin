a00beae9e575debc41be609440a8f1789ac88e24
rrhmn <raissarahmani18@gmail.com>
2025-06-15 19:32:32 +0700

    fix: minor bug

--- internal/middlewares/cors.go
+++ internal/middlewares/cors.go
@@ -8,22 +8,22 @@ import (
 )
 
 func (m *Middleware) CORSMiddleware(ctx *gin.Context) {
-	// setup whitelist origin
 	whitelistOrigin := []string{"http://localhost:5173", "https://tickitz-react.vercel.app"}
 	origin := ctx.GetHeader("Origin")
-	// log.Println("[DEBUG] Origin: ", origin)
-	allowedOrigin := ""
-	if slices.Contains(whitelistOrigin, origin) {
-		allowedOrigin = origin
-	}
-	ctx.Header("Access-Control-Allow-Origin", allowedOrigin)
-	ctx.Header("Access-Control-Allow-Methods", "GET, POST, HEAD, PATCH, PUT, DELETE, OPTIONS")
-	ctx.Header("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept")
-	ctx.Header("Access-Control-Allow-Credentials", "true")
 
-	// handle preflight
-	if ctx.Request.Method == http.MethodOptions {
-		ctx.AbortWithStatus(http.StatusNoContent)
+	if slices.Contains(whitelistOrigin, origin) {
+		ctx.Header("Access-Control-Allow-Origin", origin)
+		ctx.Header("Access-Control-Allow-Methods", "GET, POST, HEAD, PATCH, PUT, DELETE, OPTIONS")
+		ctx.Header("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept")
+		ctx.Header("Access-Control-Allow-Credentials", "true")
+
+		if ctx.Request.Method == http.MethodOptions {
+			ctx.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+	} else {
+		// Origin not allowed
+		ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "CORS origin denied"})
 		return
 	}
 
